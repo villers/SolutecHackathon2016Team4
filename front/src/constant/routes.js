e169e3d4b761efe9cce2dce227abb4d4d0b618bb
kMeillet <robin.meillet@epitech.eu>
2016-06-25 16:09:24 +0200

    Authentication good right

--- front/src/constant/routes.js
+++ front/src/constant/routes.js
@@ -1,14 +1,43 @@
+const skip = ($state, $auth) =>
+  new Promise(resolve => {
+    if ($auth.isAuthenticated()) {
+      resolve();
+    } else {
+      $state.go('login');
+    }
+  });
+
+const authenticator = ($q, $location, $auth) => {
+  const deferred = $q.defer();
+
+  if ($auth.isAuthenticated()) {
+    deferred.resolve();
+  } else {
+    $location.path('/login');
+  }
+
+  return deferred.promise;
+};
+
 export const ROUTES = {
   default: '/',
-  login: {
-    name: 'login',
-    url: '/auth',
-    template: '<login></login>',
-  },
   dashboard: {
     name: 'home',
     url: '/',
     template: '<home></home>',
+    resolve: { authenticator },
+  },
+  login: {
+    name: 'login',
+    url: '/auth',
+    template: '<login></login>',
+    resolve: { authenticator },
+  },
+  register: {
+    name: 'register',
+    url: '/register',
+    template: '<register></register>',
+    resolve: { skip },
   },
 };
 
