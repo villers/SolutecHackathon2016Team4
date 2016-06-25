ef5ef4eafb586d99511bbca024abe25357c23c21
sekour <sekour.mennad@epitech.eu>
2016-06-25 18:32:20 +0200

    user controller

--- api/app/Http/Controllers/UsersController.php
+++ api/app/Http/Controllers/UsersController.php
@@ -19,6 +19,39 @@ class UsersController extends Controller
         $user = User::all();
 
         return response()->json(compact('user'));
+    }
 
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $user->type = $request['type'];
+        $user->points = $request['points'];
+        $user->last_name = $request['last_name'];
+        $user->first_name = $request['first_name'];
+        $user->login = $request['login'];
+        $user->email = $request['email'];
+        $user->password = $request['password'];
+        $user->country = $request['country'];
+        $user->city = $request['city'];
+        $user->postal_code = $request['postal_code'];
+        $user->address_number = $request['address_number'];
+        $user->address = $request['address'];
+        $user->graduation = $request['graduation'];
+        $user->can_drive = $request['can_drive'];
+
+        $user->save();
+
+        return response()->json(['status' => 'true', 'message' => 'user édité !']);
+    }
+
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        $user->delete();
+
+        return response()->json(['status' => 'true', 'message' => 'User supprimé !']);
     }
 }
