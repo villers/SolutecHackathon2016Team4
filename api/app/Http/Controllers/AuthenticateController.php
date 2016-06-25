0dea5581c56d87dd1120d1ad63110cd689eb66a3
mohamed benfriha <patchouni@MacBook-Pro-de-mohamed.local>
2016-06-25 11:57:01 +0200

    ajout authentification

---
+++ api/app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\User;
+
+
+class AuthenticateController extends Controller
+{
+    public function __construct()
+    {
+        // Apply the jwt.auth middleware to all methods in this controller
+        // except for the authenticate method. We don't want to prevent
+        // the user from retrieving their token if they don't already have it
+        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+    }
+
+    public function index()
+    {
+        // Retrieve all the users in the database and return them
+        $users = User::all();
+        return $users;
+    }
+
+
+    /**
+     * Return a JWT
+     **/
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        try {
+            // verify the credentials / create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['status'=> false, 'error' => 'Ce compte n\'existe pas ou les informations sont erronées'], 401);
+            }
+        } catch (JWTException $e) {
+            // when wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        // if no errors return a JWT
+        $status = true;
+        return response()->json(compact('status', 'token', 'credentials'));
+    }
+
+    public function register(Request $request)
+    {
+        $credentials = $request->only('first_name', 'last_name', 'login', 'email', 'password', 'country', 'city', 'postal_code', 'address_number', 'adress');
+
+        
+    }
+}
