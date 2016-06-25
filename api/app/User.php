fb70357c3b0e5b4ca9ff16439ae2254dd64519a2
mohamed benfriha <patchouni@MacBook-Pro-de-mohamed.local>
2016-06-25 15:32:30 +0200

    error model user

--- api/app/User.php
+++ api/app/User.php
@@ -9,17 +9,13 @@ class User extends Authenticatable
     // The attributes that are mass assignable.
 
     protected $fillable = [
-<<<<<<< HEAD
-    'type', 'points', 'last_name', 'first_name','login','email','password','country','city','postal_code','adresse_number','adress','is_active','token_active','graduation','lang','can_drive',
-=======
-        'login', 'email', 'password', 'first_name', 'last_name', 'country', 'city', 'postal_code', 'address_number', 'address', 'type', 'is_active', 'token_active',
->>>>>>> 7a08798dd8f69906f789342760434158de9d77bd
+        'type', 'points', 'last_name', 'first_name','login','email','password','country','city','postal_code','adresse_number','adress','is_active','token_active','graduation','lang','can_drive',
     ];
 
     // The attributes that should be hidden for arrays.
 
     protected $hidden = [
-    'password',
+        'password',
     ];
 
     public function notifications()
