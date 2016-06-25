d9d4ed25d4125c73e33cdff274ce6ccfd82c4b24
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-06-25 14:53:25 +0200

    update api url

--- front/src/constant/config.js
+++ front/src/constant/config.js
@@ -1,5 +1,5 @@
 export const ENV = 'PRODUCTION';
 export const DEBUG = (ENV !== 'PRODUCTION');
-export const API_URL = 'http://192.168.101.3:8000/api';
+export const API_URL = 'http://192.168.101.3:8000';
 
 export default { ENV, DEBUG, API_URL };
