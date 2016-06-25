97d43e1e22cd0012deb0012295b000099c776ac1
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-06-25 14:47:20 +0200

    add auth login

--- front/src/app.main.js
+++ front/src/app.main.js
@@ -4,25 +4,46 @@ import material from 'angular-material';
 import storage from 'angular-storage';
 import animate from 'angular-animate';
 import aria from 'angular-aria';
+import messages from 'angular-messages';
+import satellizer from 'satellizer';
 
 import services from './app.services';
 import components from './app.components';
 import directives from './app.directives';
 
 import ROUTES from './constant/routes';
+import CONFIG from './constant/config';
 
 const moduleName = 'app';
 
 angular
-  .module(moduleName, [router, aria, animate, storage, material, services, components, directives])
-  .config(($stateProvider, $locationProvider, $urlRouterProvider) => {
+  .module(moduleName, [
+    router,
+    aria,
+    animate,
+    storage,
+    material,
+    messages,
+    satellizer,
+    services,
+    components,
+    directives,
+  ])
+  .config(($stateProvider, $locationProvider, $urlRouterProvider, $authProvider) => {
     $locationProvider.html5Mode(true);
+
+    // Redirect to default state if any other states
     $urlRouterProvider.otherwise(ROUTES.default);
 
+    // Load route from constant/routes.js
     Object
       .keys(ROUTES)
       .filter(route => route !== 'default')
       .forEach(route => $stateProvider.state(route, ROUTES[route]));
+
+    // Satellizer configuration that specifies which API
+    $authProvider.baseUrl = CONFIG.API_URL;
+    $authProvider.loginUrl = '/api/authenticate';
   });
 
 export default moduleName;
--- front/src/component/login/controller/login.controller.js
+++ front/src/component/login/controller/login.controller.js
@@ -1,11 +1,27 @@
 const SERVICES = new Map();
 
 class Login {
-  constructor($state) {
-    SERVICES.set('$state', $state);
+
+
+  constructor($state, $auth) {
+    SERVICES
+      .set('$state', $state)
+      .set('$auth', $auth);
+
+    this.credentials = {
+      email: '',
+      password: '',
+    };
+  }
+
+  login() {
+    SERVICES.get('$auth').login(this.credentials).then(() => {
+      // If login is successful, redirect to the users state
+      SERVICES.get('$state').go('home', {});
+    });
   }
 }
 
-Login.$inject = ['$state'];
+Login.$inject = ['$state', '$auth'];
 
 export default Login;
