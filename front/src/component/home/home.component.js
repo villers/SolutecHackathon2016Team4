2d62602c606c75da8e1100342b13d0fae50e10f1
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-06-25 12:07:42 +0200

    Change layout

---
+++ front/src/component/home/home.component.js
@@ -0,0 +1,10 @@
+import controller from './controller/home.controller';
+import template from './template/home.html';
+import './style/home.styl';
+
+export default {
+  bindings: {},
+  controllerAs: 'vm',
+  controller,
+  template,
+};
---
+++ front/src/component/login/login.component.js
@@ -0,0 +1,10 @@
+import controller from './controller/login.controller';
+import template from './template/login.html';
+import './style/login.styl';
+
+export default {
+  bindings: {},
+  controllerAs: 'vm',
+  controller,
+  template,
+};
