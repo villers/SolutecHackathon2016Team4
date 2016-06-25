e39be8f79a8e670a3e8a1a9461a6d3f6067fe7e8
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-06-25 17:02:13 +0200

    update theme

--- front/src/component/toolbar/toolbar.component.js
+++ front/src/component/toolbar/toolbar.component.js
@@ -1,6 +1,8 @@
 import controller from './controller/toolbar.controller';
 import template from './template/toolbar.html';
 import './style/toolbar.styl';
+import './img/logo.svg';
+import './img/typo.svg';
 
 export default {
   bindings: {},
