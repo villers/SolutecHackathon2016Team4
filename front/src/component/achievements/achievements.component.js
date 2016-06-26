b890b23d5aa587dba7f3549e6d934dee9947c7a7
Lucas STRUBI <Lucas STRUBI>
2016-06-26 09:22:28 +0200

    achievements, achievements svg, css

--- front/src/component/achievements/achievements.component.js
+++ front/src/component/achievements/achievements.component.js
@@ -1,6 +1,9 @@
 import controller from './controller/achievements.controller';
 import template from './template/achievements.html';
 import './style/achievements.styl';
+import '../../constant/img/1.svg';
+import '../../constant/img/2.svg';
+import '../../constant/img/3.svg';
 
 export default {
   bindings: {},
