dd228baa9078913793e892847c89e9745081f2b2
Lucas STRUBI <Lucas STRUBI>
2016-06-26 13:04:52 +0200

    comment

--- front/src/component/notifications/controller/notifications.controller.js
+++ front/src/component/notifications/controller/notifications.controller.js
@@ -1,18 +1,25 @@
 const SERVICES = new Map();
 
 class Notifications {
-  constructor($state, usersService) {
+  constructor($state, usersService, notificationsService) {
     SERVICES
       .set('$state', $state)
-      .set('usersService', usersService);
+      .set('usersService', usersService)
+      .set('notificationsService', notificationsService);
 
     this.notifications = [];
     SERVICES.get('usersService').me().then((result) => {
       this.notifications = result.data.notifications;
     });
   }
+  update(id) {
+    console.log(id);
+    SERVICES.get('notificationsService').update(id).then((result) => {
+      this.notifications = result.data.notifications;
+    });
+  }
 }
 
-Notifications.$inject = ['$state', 'usersService'];
+Notifications.$inject = ['$state', 'usersService', 'notificationsService'];
 
 export default Notifications;
